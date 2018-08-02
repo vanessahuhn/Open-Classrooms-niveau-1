5257607611bce1bd99bd4ed5f3792b6a5bcf6c4a
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-02 17:05:37 +0200

    première méthode helloWorld

---
+++ src/helloWorld/Main.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System  est une class. Il contient un accès à l'entrée standard, la sortie standard et la sortie d'erreur.  out  permet d'accéder à la sortie standard. On a alors accès à différentes fonctionnalités dont println. Cette fonction sert à afficher le texte qu'on lui donne entre parenthèses ( et  ) , et l'affiche dans la console. */
+		System.out.println("Hello OpenClassrooms!");
+	}
+
+}
